5a9967299d0dcdc8f943da0c93227a1063c57213
Oscar Rundh <oscar.rundh@schibsted.com>
2022-08-13 10:25:10 +0200

    add animations

---
+++ src/routes/animations/annoying-bounce/index.svelte
@@ -0,0 +1,86 @@
+<script>
+	import SyntaxHighlighting from '$lib/components/SyntaxHighlighting.svelte';
+
+	let animationRunning = false;
+</script>
+
+<svelte:head>
+	<title>Bounce animation - Tips & tricks!</title>
+	<meta
+		name="description"
+		content="This is an annoying bounce animation to let the user know they need to take action on something. - A collection fun stuff
+		built with Svelte, useful libraries, frameworks and copy/paste utils."
+	/>
+</svelte:head>
+
+<article>
+	<section>
+		<h2>Annoying bounce animation</h2>
+		<p>
+			This is an annoying bounce animation to let the user know they need to take action on
+			something.
+		</p>
+	</section>
+	<section>
+		<button
+			class:annoyingBounce={animationRunning}
+			on:click={() => (animationRunning = true)}
+			on:animationend={() => (animationRunning = false)}
+		>
+			Play animation
+		</button>
+		<SyntaxHighlighting language="css"
+			>{`@keyframes annoying-bounce {
+	0% {
+		transform: translateY(0);
+	}
+	6% {
+		transform: translateY(-16%);
+	}
+	12% {
+		transform: translateY(6%);
+	}
+	18% {
+		transform: translateY(-12%);
+	}
+	24% {
+		transform: translateY(0);
+	}
+	100% {
+		transform: translateY(0);
+	}
+}
+
+.annoyingBounce {
+	animation: annoying-bounce 1s 2 ease-in-out;
+}`}</SyntaxHighlighting
+		>
+	</section>
+</article>
+
+<style>
+	@keyframes annoying-bounce {
+		0% {
+			transform: translateY(0);
+		}
+		6% {
+			transform: translateY(-16%);
+		}
+		12% {
+			transform: translateY(6%);
+		}
+		18% {
+			transform: translateY(-12%);
+		}
+		24% {
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+
+	.annoyingBounce {
+		animation: annoying-bounce 2s 2;
+	}
+</style>
---
+++ src/routes/animations/attention-shake/index.svelte
@@ -0,0 +1,95 @@
+<script>
+	import SyntaxHighlighting from '$lib/components/SyntaxHighlighting.svelte';
+
+	let animattionRunning = false;
+</script>
+
+<svelte:head>
+	<title>Shake animation - Tips & tricks!</title>
+	<meta
+		name="description"
+		content="A shaking animation to grab attention. - A collection fun stuff
+		built with Svelte, useful libraries, frameworks and copy/paste utils."
+	/>
+</svelte:head>
+
+<article>
+	<section>
+		<h2>Attention shake animation</h2>
+		<p>This is a shaking animation to grab attention.</p>
+	</section>
+	<section>
+		<button
+			class:attentionShake={animattionRunning}
+			on:click={() => (animattionRunning = true)}
+			on:animationend={() => (animattionRunning = false)}
+		>
+			Play animation
+		</button>
+		<SyntaxHighlighting language="css"
+			>{`@keyframes attention-shake {
+	0% {
+		transform: rotate(0deg) scale(1);
+	}
+	25% {
+		transform: rotate(0deg) scale(1.4);
+	}
+	30% {
+		transform: rotate(10deg) scale(1.4);
+	}
+	40% {
+		transform: rotate(-10deg) scale(1.4);
+	}
+	50% {
+		transform: rotate(10deg) scale(1.4);
+	}
+	55% {
+		transform: rotate(0deg) scale(1.4);
+	}
+	70% {
+		transform: rotate(0deg) scale(1.4);
+	}
+	100% {
+		transform: rotate(0deg) scale(1);
+	}
+}
+
+.attentionShake {
+	animation: attention-shake 1s 1 ease-in-out;
+}`}</SyntaxHighlighting
+		>
+	</section>
+</article>
+
+<style>
+	@keyframes attention-shake {
+		0% {
+			transform: rotate(0deg) scale(1);
+		}
+		25% {
+			transform: rotate(0deg) scale(1.4);
+		}
+		30% {
+			transform: rotate(10deg) scale(1.4);
+		}
+		40% {
+			transform: rotate(-10deg) scale(1.4);
+		}
+		50% {
+			transform: rotate(10deg) scale(1.4);
+		}
+		55% {
+			transform: rotate(0deg) scale(1.4);
+		}
+		70% {
+			transform: rotate(0deg) scale(1.4);
+		}
+		100% {
+			transform: rotate(0deg) scale(1);
+		}
+	}
+
+	.attentionShake {
+		animation: attention-shake 1s 1 ease-in-out;
+	}
+</style>
