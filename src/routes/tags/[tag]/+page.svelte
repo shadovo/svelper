457b31a822c395f6d7e5f756b595ef9f5cf0e641
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2022-08-20 10:46:34 +0200

    remove tags test

--- src/routes/tags/[tag]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let data = {
-		title: 'not found',
-	};
-
-	console.log(data);
-</script>
-
-<h3>{data?.title}</h3>
--- src/routes/tags/[tag]/+page.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export function load({ params }) {
-	console.log(params.tag);
-	return {
-		title: params.tag,
-	};
-}
