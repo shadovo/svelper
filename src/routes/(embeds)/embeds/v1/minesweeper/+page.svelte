ce3e0755eeec030c5cd0c61bc926fa61731a61d3
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-03-12 18:52:24 +0100

    fix(embed/minesweeper): only access query params in browser

--- src/routes/(embeds)/embeds/v1/minesweeper/+page.svelte
+++ src/routes/(embeds)/embeds/v1/minesweeper/+page.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import Minesweeper from '$lib/games/Minesweeper.svelte';
 
-	const columns = $page.url.searchParams.get('columns') ?? undefined;
-	const rows = $page.url.searchParams.get('rows') ?? undefined;
-	const mines = $page.url.searchParams.get('mines') ?? undefined;
+	let columns: string | undefined;
+	let rows: string | undefined;
+	let mines: string | undefined;
+
+	if (browser) {
+		columns = $page.url.searchParams.get('columns') ?? undefined;
+		rows = $page.url.searchParams.get('rows') ?? undefined;
+		mines = $page.url.searchParams.get('mines') ?? undefined;
+	}
 </script>
 
-<Minesweeper {columns} {rows} {mines} />
+{#if browser}
+	<Minesweeper {columns} {rows} {mines} />
+{/if}
