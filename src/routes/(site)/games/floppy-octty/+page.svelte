425a5f4728a96570769a2b0bc95902b77d2743de
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-03-28 21:14:52 +0200

    feat(game/floppy-octty): add game floppy octty

---
+++ src/routes/(site)/games/floppy-octty/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import FloppyOctty from '$lib/games/FloppyOctty.svelte';
+</script>
+
+<svelte:head>
+	<title>Floppy Octty as Svelte component - Svelper</title>
+	<meta
+		name="description"
+		content="A simple game inspired by the classic Flappy Bird game built as a Svelte component using
+		canvas and emojies 🐙.
+		- A collection fun stuff"
+	/>
+</svelte:head>
+
+<section>
+	<h2>FLOPPY OCTTY!</h2>
+	<p>
+		A simple game inspired by the classic Flappy Bird game built as a Svelte component using
+		<code>&lt;canvas&gt;</code> and emojies 🐙.
+	</p>
+</section>
+<section>
+	<FloppyOctty />
+</section>
