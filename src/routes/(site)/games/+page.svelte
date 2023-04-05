4de8108367895adf2dffe075754ac5eaddf2c08f
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-04-05 16:26:08 +0200

    feat: collection pages for games and tools

---
+++ src/routes/(site)/games/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Games built in Svelte - Svelper</title>
+	<meta name="description" content="A collection games built with Svelte." />
+</svelte:head>
+
+<section class="content-width">
+	<h2>Games built in Svelte</h2>
+	<div class="grid">
+		<a class="card" href="/games/minesweeper">
+			<p class="icon">💣</p>
+			<h3>Minesweeper</h3>
+			<p>The classic game Minesweeper as Svelte component.</p>
+		</a>
+		<a class="card" href="/games/floppy-octty">
+			<p class="icon">🐙</p>
+			<h3>Floppy Octty</h3>
+			<p>Underwater version of the classic Flappy Bird game.</p>
+		</a>
+	</div>
+</section>
+
+<style lang="scss">
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(3, minmax(200px, 1fr));
+		gap: var(--gap);
+	}
+
+	.card {
+		background: var(--color-bg);
+		box-shadow: 0 2px 3px 1px rgb(0, 0, 0, 0.3);
+		padding: 1rem;
+		border: 1px solid var(--c-text);
+		background: var(--c-table-row);
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		text-decoration: none;
+		color: var(--color-text);
+		transform-origin: center;
+
+		&:hover {
+			transform: scale(1.02);
+			box-shadow: 0 3px 6px 1px rgb(0, 0, 0, 0.3);
+		}
+
+		.icon {
+			text-align: center;
+			font-size: 4rem;
+			margin-bottom: 0.5rem;
+			padding: var(--gap-half) 0;
+		}
+
+		h3 {
+			margin-bottom: 0.5rem;
+			font-weight: bold;
+		}
+		p {
+			font-weight: normal;
+		}
+	}
+
+	@media (max-width: 768px) {
+		.grid {
+			grid-template-columns: repeat(2, minmax(200px, 1fr));
+		}
+	}
+
+	@media (max-width: 480px) {
+		.grid {
+			grid-template-columns: repeat(1, minmax(200px, 1fr));
+		}
+	}
+</style>
