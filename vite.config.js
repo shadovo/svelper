4cefdda205811f59b0b183515bd4219bd2939501
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-04-07 16:55:10 +0200

    fix: hardcoded image upscale

--- vite.config.js
+++ vite.config.js
@@ -12,22 +12,9 @@ export default defineConfig(() => {
 				defaultDirectives: (url) => {
 					const extension = url.pathname.substring(url.pathname.lastIndexOf('.') + 1);
 					if (supportedExtensions.includes(extension)) {
-						const upscaledWidths = (url.searchParams.get('scale-width') || '')
-							.split(';')
-							.map((w) => parseInt(w, 10))
-							.reduce((acc, w) => {
-								acc.push(w);
-								acc.push(w * 2);
-								acc.push(w * 3);
-								return acc;
-							}, [])
-							.flat()
-							.sort((a, b) => b - a)
-							.join(';');
 						return new URLSearchParams({
 							format: 'webp;' + extension,
 							picture: true,
-							...(upscaledWidths.length > 0 ? { w: upscaledWidths } : {}),
 						});
 					}
 					return new URLSearchParams();
