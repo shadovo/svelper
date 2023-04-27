7eb8d73e6a2d4eaa17e32d5667037d3903e4a486
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-04-27 17:47:25 +0200

    feat: add end condition ro hardbread

--- src/lib/games/hardbread-ship/HardbreadShip.svelte
+++ src/lib/games/hardbread-ship/HardbreadShip.svelte
@@ -6,47 +6,97 @@
 	import shipImage from './assets/hardbread-ship.png?w=333&imagetools';
 	import backgroundImage from './assets/hardbread-background.png?w=1024&imagetools';
 
+	type GameStatus = 'notstarted' | 'playing' | 'won' | 'lost';
+
+	type Game = {
+		status: GameStatus;
+		currentTimer: string;
+		startTime: Date | null;
+		endTime: Date | null;
+	};
+
 	const heroImageSize = `
 		(min-width: 1024px)	688px,
 		(min-width: 769px) calc(100vw - 336px),
 		calc(100vw - 16px)
 	`;
 
+	const MAX_LEAN = 35;
+
 	let gameRef: HTMLDivElement;
 	let gameScale = 1;
-
-	// let lean = tweened(30, {
-	// 	duration: 3000,
-	// });
-
+	let game: Game;
 	let lean = 0;
+
+	let shipY = tweened(0, {
+		duration: 2000,
+	});
+	let shipX = tweened(0, {
+		duration: 2000,
+	});
+	let waterOpacity = tweened(1, {
+		delay: 1000,
+		duration: 2000,
+	});
+
+	// function that gets a rotation in deg and calculates the required x and y to move the object straight down
+	function getXYFromRotation(rotation: number) {
+		const radians = (rotation * Math.PI) / 180;
+		const x = Math.sin(radians);
+		const y = Math.cos(radians);
+		return { x, y };
+	}
 
 	function handleOrientation(event: DeviceOrientationEvent) {
 		// const absolute = event.absolute;
 		// const alpha = event.alpha;
 		// const beta = event.beta;
 		// const gamma = event.gamma;
-		lean = event.gamma || 0;
+		if (game?.status === 'playing') {
+			lean = event.gamma || 0;
 
-		// Do stuff with the new orientation data
-		console.log('orientation', event);
+			// Do stuff with the new orientation data
+			console.log('orientation', event);
+		}
+	}
+
+	function updateTimer() {
+		if (game.status === 'playing') {
+			const seconds = Math.floor((new Date().getTime() - (game.startTime?.getTime() || 0)) / 1000);
+			const minutes = Math.floor(seconds / 60);
+			const secondsLeft = seconds % 60;
+			const secondsString = secondsLeft < 10 ? `0${secondsLeft}` : `${secondsLeft}`;
+			const minutesString = minutes < 10 ? `0${minutes}` : `${minutes}`;
+			game.currentTimer = `${minutesString}:${secondsString}`;
+		}
+		setTimeout(updateTimer, 1000);
+	}
+
+	function createGame() {
+		game = {
+			status: 'playing',
+			currentTimer: '00:00',
+			startTime: new Date(),
+			endTime: null,
+		};
+
+		updateTimer();
 	}
 
 	onMount(() => {
-		// console.log('on mount');
-		// const updateLeanInterval = setInterval(() => {
-		// 	console.log('update lean');
-		// 	lean.update((v) => -1 * v);
-		// }, 3000);
-		// return () => {
-		// 	console.log('on destroy');
-		// 	clearInterval(updateLeanInterval);
-		// };
-		console.log(gameRef.parentElement?.clientWidth);
 		gameScale = (gameRef.parentElement?.clientWidth || 512) / 512;
-
 		window.addEventListener('deviceorientation', handleOrientation, true);
 	});
+
+	$: {
+		if (Math.abs(lean) > MAX_LEAN) {
+			game.status = 'lost';
+			const { x, y } = getXYFromRotation(lean);
+			shipY.set(y * 500);
+			shipX.set(x * 500);
+			waterOpacity.set(0);
+		}
+	}
 </script>
 
 <div bind:this={gameRef} class="game" style="transform: scale({gameScale})">
@@ -61,7 +111,7 @@
 		/>
 	</div>
 
-	<div class="ship" style="transform: rotate({lean}deg)">
+	<div class="ship" style="transform: rotate({lean}deg) translate({$shipX}px, {$shipY}px)">
 		<Image
 			alt=""
 			role="presentation"
@@ -72,7 +122,7 @@
 		/>
 	</div>
 
-	<div class="water">
+	<div class="water" style="opacity: {$waterOpacity}">
 		<Image
 			alt=""
 			role="presentation"
@@ -82,6 +132,16 @@
 			background="transparent"
 		/>
 	</div>
+	<div class="warning" style="opacity: {Math.abs(lean) / MAX_LEAN}" />
+	<div class="score-card">
+		{#if game?.status === 'playing'}
+			<p>{game.currentTimer}</p>
+		{:else if game?.status === 'lost'}
+			<p>You sank after {game.currentTimer}</p>
+		{:else}
+			<button on:click={createGame}>Start</button>
+		{/if}
+	</div>
 </div>
 
 <style>
@@ -90,11 +150,30 @@
 		height: 512px;
 		position: relative;
 		transform-origin: top left;
+		overflow: hidden;
 	}
 	.water,
 	.ship,
-	.background {
+	.background,
+	.warning,
+	.score-card {
 		position: absolute;
+	}
+
+	.warning {
+		inset: 0 0 80px 0;
+		box-shadow: inset 0 0 60px 0px red;
+	}
+
+	.score-card {
+		bottom: 0;
+		height: 80px;
+		background: #177399;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 3rem;
 	}
 
 	.water {
