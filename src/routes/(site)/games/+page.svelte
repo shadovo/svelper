8ac468d319e48046a4c6840399f2d3caa6010dc5
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-05-04 18:13:28 +0200

    fix: hide hardbread link

--- src/routes/(site)/games/+page.svelte
+++ src/routes/(site)/games/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import Article from '$lib/components/Article.svelte';
 	import ArticleGrid from '$lib/components/ArticleGrid.svelte';
-	import hardbreadHeroImage from '$img/games/hardbread.png?w=1098;936;750;732;624;500;366;312;275;250&imagetools';
+	// import hardbreadHeroImage from '$img/games/hardbread.png?w=1098;936;750;732;624;500;366;312;275;250&imagetools';
 	import minesweeperHeroImage from '$img/games/minesweeper.png?w=1098;936;750;732;624;500;366;312;275;250&imagetools';
 	import floppyOcctyHeroImage from '$img/games/floppy-octty.png?w=1098;936;750;732;624;500;366;312;275;250&imagetools';
 
 	const articles = [
-		{
-			title: 'Hardbread delivery',
-			description: 'Balance the ship as long as you can before it capsizes.',
-			image: hardbreadHeroImage,
-			imageColor: '#0f353a',
-			link: '/games/hardbread-ship',
-		},
+		// {
+		// 	title: 'Hardbread delivery',
+		// 	description: 'Balance the ship as long as you can before it capsizes.',
+		// 	image: hardbreadHeroImage,
+		// 	imageColor: '#0f353a',
+		// 	link: '/games/hardbread-ship',
+		// },
 		{
 			title: 'Floppy Octty',
 			description: 'Underwater version of the classic Flappy Bird game.',
