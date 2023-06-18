9b5fff7dd15b27cbe8f92e2549b7a641179896d8
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-06-18 15:28:01 +0200

    feat: unified lh config

---
+++ .lighthouse/lighthouserc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	ci: {
+		collect: {
+			numberOfRuns: 2,
+		},
+		assert: {
+			preset: 'lighthouse:no-pwa',
+			assertions: {
+				'uses-passive-event-listeners': 'off',
+				'csp-xss': 'off',
+				'bf-cache': 'off',
+				'color-contrast': 'off', // temporary disabled due to oklch color space issue
+			},
+		},
+		upload: {
+			target: 'temporary-public-storage',
+		},
+	},
+};
