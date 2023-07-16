3904f6d4714c9d90d4ad69d5bdcc1b95a939355a
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2023-07-16 14:41:07 +0200

    feat: add robots.txt

--- src/routes/(site)/[[tag]]/+page.ts
+++ src/routes/(site)/[[tag]]/+page.ts
@@ -1,6 +1,7 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
 import type { ArticlePromo } from '../../../data/articles.js';
 import { get as getTag, type Tag } from '../../../data/tags';
-import type { PageLoad } from './$types';
 
 export const prerender = false;
 export const ssr = true;
@@ -8,6 +9,11 @@ export const ssr = true;
 export const load = (async ({ fetch, params }) => {
 	const tag = params.tag;
 	const currentTag = tag ? getTag(tag) : undefined;
+	if (tag && !currentTag) {
+		throw error(404, {
+			message: 'The page does not exist',
+		});
+	}
 	const fetchUrl = currentTag ? `/api/feed/${currentTag.slug}` : '/api/feed';
 	const articleRequest = await fetch(fetchUrl);
 	const result = (await articleRequest.json()) as {
